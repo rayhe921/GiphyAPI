8a3337c34bfb1d45d076b9463828f341c261bc96
Ray He <rayhe921@gmail.com>
2019-10-02 22:23:27 -0700

    ajax giphy API

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,29 +1,43 @@
 
 var movies = ["The Matrix", "The Notebook", "Mr. Nobody", "The Lion King"];
 
-      // Function for displaying movie data
-      function renderButtons() {
+// Function for displaying movie data
+function renderButtons() {
 
-        $("#buttons-view").empty();
+  $("#buttons-view").empty();
 
-        for (var i = 0; i < movies.length; i++) {
+  for (var i = 0; i < movies.length; i++) {
 
-          var a = $("<button>");
-          a.addClass("movie");
-          a.attr("data-name", movies[i]);
-          a.text(movies[i]);
-          $("#buttons-view").append(a);
-        }
-      };
+    var a = $("<button>");
+    a.addClass("movie");
+    a.attr("data-name", movies[i]);
+    a.text(movies[i]);
+    $("#buttons-view").append(a);
+  }
+};
 
-      // This function handles events where one button is clicked
-      $("#add-movie").on("click", function(event) {
-        event.preventDefault();
+// This function handles events where one button is clicked
+$("#add-movie").on("click", function (event) {
+  event.preventDefault();
+  var movie = $("#movie-input").val().trim();
+  movies.push(movie);
+  renderButtons();
+});
 
-      
-        var movie = $("#movie-input").val().trim();
-        movies.push(movie);
-        renderButtons();
-      });
+renderButtons();
 
-      renderButtons();
+function giphySearch(){
+var movie = $(this).attr("data-name");
+var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=rusCH39FBwAZCCAbjio1hsKdu3nXRq6d&q="
++ movie + "&limit=10&rating=G&lang=en";
+console.log(movie);
+$.ajax({
+  url: queryURL,
+  method: "GET"
+}).then(function(response){
+  console.log("URL", queryURL);
+  console.log("response", response);
+})
+};
+
+$(document).on("click", ".movie", giphySearch);
